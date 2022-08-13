d12d39f239c487b610bf8f7de9831aff81d73f5c
Bruce Johnson <brucejo.wk@outlook.com>
2022-08-13 14:48:07 -0700

    Do not change collection on client side, avoid optimistic udate

--- packages/test-observe-change/change_common.js
+++ packages/test-observe-change/change_common.js
@@ -1,20 +1,40 @@
 export const ChangeCollection = new Mongo.Collection('change');
 
+ChangeCollection.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false
+});
+
+ChangeCollection.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true
+});
+
 let document = {
   _id: 'change',
   value: 'foo'
 };
 
-ChangeCollection.upsert({_id: 'change'}, document);
+if (Meteor.isServer) {
+  ChangeCollection.upsert({_id: 'change'}, document);
+}
 
 let otherValue = 'bar';
 
 Meteor.methods({
   change() {
-    document.value = document.value === 'foo' ? otherValue : 'foo';
-    ChangeCollection.upsert({_id: 'change'}, document);
+    if (!this.isSimulation) {
+      document.value = document.value === 'foo' ? otherValue : 'foo';
+      const update = document.value ? {$set: {value: document.value}} : {$unset: {value: ''}};
+      // ChangeCollection.upsert({_id: 'change'}, {$set: {value: document.value}});
+      ChangeCollection.upsert({_id: 'change'}, document);
+    }
   },
   changeOther() {
-    otherValue = otherValue === 'bar' ? undefined : 'bar';
+    if (!this.isSimulation) {
+      otherValue = otherValue === 'bar' ? undefined : 'bar';
+    }
   }
 });
